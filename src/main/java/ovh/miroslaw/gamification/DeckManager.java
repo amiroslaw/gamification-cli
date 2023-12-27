a2d75c64b889eefae5cf2c545d5f160bb9fd8030
amiroslaw <arek47@gmail.com>
2023-12-27 05:13:15 +0100

    fix duplicated random

--- src/main/java/ovh/miroslaw/gamification/DeckManager.java
+++ src/main/java/ovh/miroslaw/gamification/DeckManager.java
@@ -1,54 +1,61 @@
 package ovh.miroslaw.gamification;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@RequiredArgsConstructor
 @Service
 public class DeckManager {
 
-    private final Deck deck;
+    @Autowired
+    private Deck deck;
 
     public Deck draw(int drawNumber) {
-        final int deckSize = deck.getSize();
+        int deckSize = deck.getSize();
+        if (deck.getSize() < drawNumber) {
+            System.out.println("Draws reminded: " + (drawNumber - deck.getSize()));
+            drawNumber = deck.getSize();
+        }
 
-        // one się powtarzają
-        int pooledCardsAmount = (int) RandomGenerator.getDefault()
-                .ints(drawNumber, 1, deckSize)
-                .filter(n -> n <= deck.getCards().size())
-                .count();
-        System.out.println(pooledCardsAmount);
-        final ArrayList<Card> polledCards = pollCards(pooledCardsAmount);
-        // immunite
+        final List<Card> polledCards = pollCards(drawNumber);
+        System.out.println(polledCards);
+        System.out.println(polledCards.size());
         deck.getCards().removeAll(polledCards);
         deck.setSize(deckSize - drawNumber);
         return deck;
     }
 
-    private ArrayList<Card> pollCards(int amount) {
-        final List<Card> shuffled = new ArrayList<>(deck.getCards());
+    private List<Card> pollCards(int amount) {
+        List<Card> shuffled = Stream.generate(() -> new Card(0, 0, "Empty", "Sorry no award this time"))
+                .limit((long) deck.getSize() - deck.getCards().size())
+                .collect(Collectors.toList());
+        shuffled.addAll(deck.getCards());
         Collections.shuffle(shuffled);
         final ArrayDeque<Card> deque = new ArrayDeque<>(shuffled);
-        final ArrayList<Card> polled = new ArrayList<>();
-        for (int i = 0; i < amount; i++) {
-            Card card = deque.poll();
-            if (card.type() == 1){
-                deque.addLast(card);
-                card = deque.poll();
-            }
-            polled.add(card);
+
+        return Stream.generate(() -> pollCard(deque))
+                .limit(amount)
+                .filter(c -> c.type() != 0)
+                .toList();
+    }
+
+    private Card pollCard(ArrayDeque<Card> deque) {
+        Card card = deque.poll();
+        if (card.type() == 1) {
+            deque.addLast(card);
+            card = deque.poll();
         }
-        return polled;
+        return card;
     }
 
     public void list() {
         System.out.println(deck.getSize());
         System.out.println(deck.getCards());
+        System.out.println(deck.getCards().size());
     }
 }
