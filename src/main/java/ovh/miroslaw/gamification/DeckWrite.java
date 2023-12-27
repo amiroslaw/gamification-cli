2fb72c18049075f46c16922c56d0681e4525477c
amiroslaw <arek47@gmail.com>
2023-12-27 07:53:28 +0100

    override deck config

--- src/main/java/ovh/miroslaw/gamification/DeckWrite.java
+++ src/main/java/ovh/miroslaw/gamification/DeckWrite.java
@@ -3,12 +3,12 @@ package ovh.miroslaw.gamification;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
-import lombok.SneakyThrows;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,17 +16,18 @@ import java.util.Map;
 public class DeckWrite {
 
     @Value("${spring.config.import}")
-    private String configPath;
-    @Value("${path.config}")
-    private String configDir;
+    private File configPath;
 
-    @SneakyThrows
     @RegisterReflectionForBinding(Deck.class)
     public void write(Deck deck) {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(Feature.WRITE_DOC_START_MARKER));
         Map<String, Deck> map = new HashMap<>();
         map.put("deck", deck);
-        // change path
-        mapper.writeValue(new File(configDir + "/deckOutput.yaml"), map);
+        try {
+            mapper.writeValue(configPath, map);
+        } catch (IOException e) {
+            System.out.println("Could not save deck: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
