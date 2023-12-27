686f26cd5207121fee150fed34a9d86e4c3398b2
amiroslaw <arek47@gmail.com>
2023-12-27 06:59:40 +0100

    refactoring; add printing

--- src/main/java/ovh/miroslaw/gamification/Deck.java
+++ src/main/java/ovh/miroslaw/gamification/Deck.java
@@ -1,5 +1,6 @@
 package ovh.miroslaw.gamification;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -10,9 +11,9 @@ public final class Deck {
     private List<Card> cards;
     private int size;
 
-    public Deck(List<Card> cards, int size) {
+    public Deck(List<Card> cards, Integer size) {
         this.cards = cards;
-        this.size = size;
+        this.size = size == null ? 30 : size;
     }
 
     public List<Card> getCards() {
@@ -30,8 +31,16 @@ public final class Deck {
     public int getSize() {
         return size;
     }
+    public void drawCards(List<Card> drawnCards, int drawNumber) {
+        cards.removeAll(drawnCards);
+        size -= drawNumber;
+    }
 }
 
-record Card(int id, int type, String title, String description) {
+record Card(int id, @NotNull int type, @NotNull String title, String description, String url) {
+    Card {
+        description = description == null ? "" : description;
+        url = url == null ? "" : url;
+    }
 
 }
--- src/main/java/ovh/miroslaw/gamification/DeckManager.java
+++ src/main/java/ovh/miroslaw/gamification/DeckManager.java
@@ -1,52 +1,68 @@
 package ovh.miroslaw.gamification;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@RequiredArgsConstructor
 @Service
 public class DeckManager {
 
-    @Autowired
-    private Deck deck;
+    public static final int EMPTY_CARD_VALUE = 0;
+    public static final String EMPTY_CARD_DESC = "Sorry no award this time";
+    public static final String EMPTY_CARD_TITLE = "Empty";
+    public static final int MAIN_AWARD_CARD_TYPE = 1;
+    private final Deck deck;
+
+    private final Supplier<Card> createEmptyCard = () -> new Card(EMPTY_CARD_VALUE, EMPTY_CARD_VALUE, EMPTY_CARD_TITLE, EMPTY_CARD_DESC,
+            Strings.EMPTY);
+
+    private final Consumer<Card> printCard = d -> System.out.printf("Type: %d  \t %s - %s %s%n", d.type(), d.title(), d.description(), d.url());
 
     public Deck draw(int drawNumber) {
-        int deckSize = deck.getSize();
-        if (deck.getSize() < drawNumber) {
-            System.out.println("Draws reminded: " + (drawNumber - deck.getSize()));
-            drawNumber = deck.getSize();
-        }
-
+        drawNumber = validateDrawAmount(drawNumber);
         final List<Card> polledCards = pollCards(drawNumber);
-        System.out.println(polledCards);
-        System.out.println(polledCards.size());
-        deck.getCards().removeAll(polledCards);
-        deck.setSize(deckSize - drawNumber);
+        polledCards.forEach(printCard);
+        deck.drawCards(polledCards, drawNumber);
         return deck;
     }
 
-    private List<Card> pollCards(int amount) {
-        List<Card> shuffled = Stream.generate(() -> new Card(0, 0, "Empty", "Sorry no award this time"))
+    private int validateDrawAmount(int numCardsToDraw) {
+        if (deck.getSize() < numCardsToDraw) {
+            System.out.println("Draws reminded: " + (numCardsToDraw - deck.getSize()));
+            return deck.getSize();
+        }
+        return numCardsToDraw;
+    }
+
+    private List<Card> pollCards(int numCards) {
+        final ArrayDeque<Card> deque = createDeque();
+
+        return Stream.generate(() -> pollCard(deque))
+                .limit(numCards)
+                .toList();
+    }
+
+    private ArrayDeque<Card> createDeque() {
+        List<Card> shuffled = Stream.generate(createEmptyCard)
                 .limit((long) deck.getSize() - deck.getCards().size())
                 .collect(Collectors.toList());
         shuffled.addAll(deck.getCards());
         Collections.shuffle(shuffled);
-        final ArrayDeque<Card> deque = new ArrayDeque<>(shuffled);
-
-        return Stream.generate(() -> pollCard(deque))
-                .limit(amount)
-                .filter(c -> c.type() != 0)
-                .toList();
+        return new ArrayDeque<>(shuffled);
     }
 
     private Card pollCard(ArrayDeque<Card> deque) {
         Card card = deque.poll();
-        if (card.type() == 1) {
+        if (card.type() == MAIN_AWARD_CARD_TYPE) {
             deque.addLast(card);
             card = deque.poll();
         }
@@ -54,8 +70,8 @@ public class DeckManager {
     }
 
     public void list() {
-        System.out.println(deck.getSize());
-        System.out.println(deck.getCards());
-        System.out.println(deck.getCards().size());
+        System.out.println("Deck size: " + deck.getSize());
+        System.out.printf("Cards reminds - %d:%n", deck.getCards().size());
+        deck.getCards().forEach(printCard);
     }
 }
